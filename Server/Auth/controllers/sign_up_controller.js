596b65baec575562844c43050513a13f2007af3d
Ilya <Ptkvzr_YbrekbyfH@mail.ru>
2024-11-13 10:41:40 +0300

    Revert "1.0.0 service version"
    This reverts commit 2572e19dbcffff7731fb1eddcffec77f3ec14817.

--- Server/Auth/controllers/sign_up_controller.js
+++
@@ -1,8 +0,0 @@
-exports.sign_up_default =async (req, res) => {
-    const a = await fetch("http://localhost:3000/users/create", {
-        method: "POST",
-        body: JSON.stringify(req.body)
-    })
-    const user = await a.json();
-    res.status(200).json(user);
-}
--- Server/Auth/routes/sign_up_routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const sign_up_controller = require("../controllers/sign_up_controller.js");
-const sign_up_router = express.Router();
-
-sign_up_router.post("/sign_up", sign_up_controller.sign_up_default);
-
-module.exports = sign_up_router;
