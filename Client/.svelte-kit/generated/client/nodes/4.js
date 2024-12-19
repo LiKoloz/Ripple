c3ffc45fd53d607479c683d2916ca02bcc584d95
Ilya <Ptkvzr_YbrekbyfH@mail.ru>
2024-12-19 18:11:55 +0300

    jectii_commit_all_client

---
+++ Client/.svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/profile/[user_Id]/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/profile/[user_Id]/+page.svelte";
