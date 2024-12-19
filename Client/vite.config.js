1943f9ff250ddf7575e9e1599064ab0a8a6a6ddf
Ilya <Ptkvzr_YbrekbyfH@mail.ru>
2024-12-19 22:55:05 +0300

    jhgk
    jkh

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		host: 'http://217.114.0.104',  // Укажите нужный IP-адрес
-		port: 4173,              // Укажите нужный порт
+		host: '217.114.0.104',  
+		port: 4173,            
 	  }
 });
